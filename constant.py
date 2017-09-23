5ced8d1c8bc7aeec2dee9f145a0c287b7cf3a1be
Juan Manuel Rosero Puente <snowblow07@gmail.com>
2017-09-23 00:59:34 +0000

    order of growth BigO notation examples

---
+++ constant.py
@@ -0,0 +1,5 @@
+def c_to_f(c):
+    return c * 9.0 / 5 + 32
+
+print(c_to_f(17))
+
---
+++ logarithmic.py
@@ -0,0 +1,12 @@
+def intToStr(i):
+    digits = "0123456789"
+    if i == 0:
+        return "0"
+    result = ""
+    while i > 0:
+        result = digits[i % 10] + result
+        i //= 10
+    return result
+
+print(intToStr(777))
+
